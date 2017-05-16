e78dac8545c2848e067b9901e653717a32b098f5
MarkChang <shea4a71@gmail.com>
2017-05-16 10:05:18 +0800

    temp

--- Draw/Draw2ViewController.m
+++ Draw/Draw2ViewController.m
@@ -10,7 +10,7 @@
 
 #import "Draw2ViewController.h"
 
-@interface Draw2ViewController ()
+@interface Draw2ViewController () <CAAnimationDelegate>
 
 @end
 
@@ -30,27 +30,27 @@
     [super viewDidAppear:animated];
 
     // Set up the shape of the circle
-    int radius = 100;
+    int radius = 70;
     CAShapeLayer *circle = [CAShapeLayer layer];
     // Make a circular shape
     circle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 2.0*radius, 2.0*radius)
                                              cornerRadius:radius].CGPath;
     // Center the shape in self.view
-    circle.position = CGPointMake(CGRectGetMidX(self.view.frame)-radius,
-                                  CGRectGetMidY(self.view.frame)-radius);
+    circle.position = CGPointMake(30, 100);
 
     // Configure the apperence of the circle
     circle.fillColor = [UIColor clearColor].CGColor;
     circle.strokeColor = [UIColor blackColor].CGColor;
-    circle.lineWidth = 5;
+    circle.lineWidth = 20;
 
     // Add to parent layer
     [self.view.layer addSublayer:circle];
 
     // Configure animation
     CABasicAnimation *drawAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    drawAnimation.duration            = 1.0; // "animate over 10 seconds or so.."
-    drawAnimation.repeatCount         = 1.0;  // Animate only once..
+    drawAnimation.delegate = self;
+    drawAnimation.duration = 1.0; // "animate over 10 seconds or so.."
+    drawAnimation.repeatCount = 1.0;  // Animate only once..
 
     // Animate from no part of the stroke being drawn to the entire stroke being drawn
     drawAnimation.fromValue = [NSNumber numberWithFloat:0.0f];
@@ -61,6 +61,14 @@
     
     // Add the animation to the circle
     [circle addAnimation:drawAnimation forKey:@"drawCircleAnimation"];
+}
+
+- (void)animationDidStart:(CAAnimation *)anim {
+    NSLog(@"動畫開始");
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    NSLog(@"動畫結束");
 }
 
 @end
